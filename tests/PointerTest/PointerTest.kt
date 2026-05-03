9fc75619552bedc0d4bcf8c22e428f00f65b5d31
Bitsy <bitsydoge@proton.me>
2026-05-03 15:38:30 +0200

    Enhance pointer and array handling in PointerTest.kt with nullable support

--- tests/PointerTest/PointerTest.kt
+++ tests/PointerTest/PointerTest.kt
@@ -1,10 +1,23 @@
 package PointerTest
 
+// =================================
+// MARK: Classes
+// =================================
+
 data class Vec2(val x: Int, val y: Int)
+data class Offset(val x: Int? = null, val y: Int = 0)
 
-data class Test(val x: Int? = null, val y: Int? = null)
+// =================================
+// MARK: Array Ptr
+// =================================
 
-fun passArray(inarr: Ptr<Array<Int>>? = null) {
+fun passArrayPtr(inarr: Ptr<Array<Int>>) {
+    for(i in 0..<inarr.size) {
+        println("inarr[$i] = ${inarr[i]}")
+    }
+}
+
+fun passArrayNullablePtr(inarr: Ptr<Array<Int>?>) {
     if (inarr == null) {
         println("inarr is null")
         return
@@ -14,7 +27,37 @@ fun passArray(inarr: Ptr<Array<Int>>? = null) {
     }
 }
 
-fun passArrayValue(inarr: Array<Int?>? = null) {
+fun passArrayPtrNullable(inarr: Ptr<Array<Int>>? = null) {
+    if (inarr == null) {
+        println("inarr is null")
+        return
+    }
+    for(i in 0..<inarr.size) {
+        println("inarr[$i] = ${inarr[i]}")
+    }
+}
+
+fun passArrayNullablePtrNullable(inarr: Ptr<Array<Int>?>? = null) {
+    if (inarr == null) {
+        println("inarr is null")
+        return
+    }
+    for(i in 0..<inarr.size) {
+        println("inarr[$i] = ${inarr[i]}")
+    }
+}
+
+// =================================
+// MARK: Array Value
+// =================================
+
+fun passArrayValue(inarr: Array<Int?>) {
+    for(i in 0..<(inarr.size)) {
+        println("inarr[$i] = ${inarr[i]}")
+    }
+}
+
+fun passArrayValueNullable(inarr: Array<Int?>? = null) {
     if (inarr == null) {
         println("inarr is null")
         return
@@ -24,7 +67,15 @@ fun passArrayValue(inarr: Array<Int?>? = null) {
     }
 }
 
-fun passVec(inVec: Vec2? = null) {
+// =================================
+// MARK: Vec Ptr
+// =================================
+
+fun passVecPtr(inVec: Ptr<Vec2>) {
+    println(inVec)
+}
+
+fun passVecPtrNullable(inVec: Ptr<Vec2>? = null) {
     if (inVec == null) {
         println("inVec is null")
         return
@@ -32,63 +83,92 @@ fun passVec(inVec: Vec2? = null) {
     println(inVec)
 }
 
-fun passVecPtr(inVec: Ptr<Vec2>? = null) {
+fun passVecNullablePtr(inVec: Ptr<Vec2?>) {
+    println(inVec)
+}
+
+fun passVecNullablePtrNullable(inVec: Ptr<Vec2?>? = null) {
     if (inVec == null) {
         println("inVec is null")
         return
     }
     println(inVec)
 }
+
+// =================================
+// MARK: Vec Value
+// =================================
+
+fun passVecValue(inVec: Vec2) {
+    println(inVec)
+}
+
+fun passVecValueNullable(inVec: Vec2? = null) {
+    if (inVec == null) {
+        println("inVec is null")
+        return
+    }
+    println(inVec)
+}
+
+// =================================
+// MARK: Main
+// =================================
 
 fun main() {
-    val array = arrayOf(1, 2, 3)
-    passArray(array.toPtr())
-    passArray()
-    passArray(null)
 
-    passArrayValue()
-    passArrayValue(null)
+    // ARRAY
+
+    val array = arrayOf(1, 2, 3)
+    val arrayNullable: Array<Int>? = arrayOf(1, 2, 3)
+
+    // ARRAY PTR
+
+    passArrayPtr(array.toPtr())
+
+    passArrayNullablePtr(array.toPtr())
+    passArrayNullablePtr(arrayNullable?.toPtr())
+
+    passArrayPtrNullable(array.toPtr())
+    passArrayPtrNullable()
+    passArrayPtrNullable(null)
+
+    passArrayNullablePtrNullable(arrayNullable?.toPtr())
+    passArrayNullablePtrNullable()
+    passArrayNullablePtrNullable(null)
+
+    // ARRAY VALUE
+
+    passArrayValue(array)
+
+    passArrayValueNullable(array)
+    passArrayValueNullable(null)
+    passArrayValueNullable()
+
+    // VEC
 
     val vec = Vec2(10,20)
-    passVec(vec)
-    passVec(null)
-    passVec()
+    val vecNullable: Vec2? = Vec2(10,20)
+
+    // VEC PTR
 
     passVecPtr(vec.toPtr())
-    passVecPtr(null)
-    passVecPtr()
 
-    val test1 = Test()
-    val test2 = Test(5,null)
-    val test3 = Test(null,null)
-    val test4 = Test(10,20)
+    passVecPtrNullable(vec.toPtr())
+    passVecPtrNullable()
+    passVecPtrNullable(null)
 
-    println(test1)
-    println(test2)
-    println(test3)
-    println(test4)
+    passVecNullablePtr(vecNullable?.toPtr())
 
-    if (test1 == test2) {
-        println("test1 == test2")
-    }
+    passVecNullablePtrNullable(vecNullable?.toPtr())
+    passVecNullablePtrNullable(null)
+    passVecNullablePtrNullable()
 
-    if (test2 == test3) {
-        println("test2 == test3")
-    }
+    // VEC VALUE
 
-    if (test3 == test4) {
-        println("test3 == test4")
-    }
+    passVecValue(vec)
 
-    if (test4 == test1) {
-        println("test4 == test1")
-    }
-
-    if (test2 == test4) {
-        println("test2 == test4")
-    }
-
-    if (test1 == test3) {
-        println("test1 == test3")
-    }
+    passVecValueNullable(vec)
+    passVecValueNullable(null)
+    passVecValueNullable()
 }
