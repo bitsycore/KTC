8da33ef0daf672cc9e22a8cf112843809d52a023
Bitsy <bitsydoge@proton.me>
2026-05-12 16:11:17 +0200

    Add integration tests for random number generation functions

---
+++ tests/RandomTest/RandomTest.kt
@@ -0,0 +1,71 @@
+package RandomTest
+
+fun testNextInt() {
+    println("--- nextInt ---")
+    println("nextInt() = ${Random.nextInt()}")
+    println("nextInt(100) = ${Random.nextInt(100)}")
+}
+
+fun testNextIntBetween() {
+    println("--- nextIntBetween ---")
+    for (i in 0 until 5) {
+        val v = Random.nextIntBetween(10, 20)
+        println("nextIntBetween(10, 20) = $v")
+        if (v < 10 || v >= 20) c.exit(1)
+    }
+}
+
+fun testNextLong() {
+    println("--- nextLong ---")
+    println("nextLong() = ${Random.nextLong()}")
+    println("nextLong(1000L) = ${Random.nextLong(1000L)}")
+}
+
+fun testNextLongBetween() {
+    println("--- nextLongBetween ---")
+    println("nextLongBetween(0L, 100L) = ${Random.nextLongBetween(0L, 100L)}")
+}
+
+fun testNextFloat() {
+    println("--- nextFloat ---")
+    for (i in 0 until 5) {
+        println("nextFloat() = ${Random.nextFloat()}")
+    }
+}
+
+fun testNextDouble() {
+    println("--- nextDouble ---")
+    for (i in 0 until 5) {
+        println("nextDouble() = ${Random.nextDouble()}")
+    }
+}
+
+fun testNextDoubleBetween() {
+    println("--- nextDoubleBetween ---")
+    for (i in 0 until 5) {
+        println("nextDoubleBetween(5.0, 10.0) = ${Random.nextDoubleBetween(5.0, 10.0)}")
+    }
+}
+
+fun testNextBoolean() {
+    println("--- nextBoolean ---")
+    var t = 0
+    var f = 0
+    for (i in 0 until 20) {
+        if (Random.nextBoolean()) t++ else f++
+    }
+    println("true=$t false=$f")
+    if (t == 0 || f == 0) c.exit(1)
+}
+
+fun main() {
+    testNextInt()
+    testNextIntBetween()
+    testNextLong()
+    testNextLongBetween()
+    testNextFloat()
+    testNextDouble()
+    testNextDoubleBetween()
+    testNextBoolean()
+    println("ALL OK")
+}
